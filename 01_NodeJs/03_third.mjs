0be8c2a9944dcd64058cbf6d181658e77d5ccc83
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-03-08 15:21:56 +0530

    Improve clarity of ES Module usage notes in third.mjs and forth.js

--- 01_NodeJs/03_third.mjs
+++ 01_NodeJs/03_third.mjs
@@ -9,7 +9,6 @@ export default add;
 
 // Now to go the forth.js file to use it.
 
-// Remember, we are using .mjs here to tell the Node that this file uses modern js module but
-// you don't need this if have any bundler (eg. vite) in you project.
-// Or simply, when you initialise the project with npm you can write the type = "module"
-// in your package.json file. Both way tells your Node to treat the file as it using morden js module.
+// Note: We are using .mjs here to tell the Node that this file is a ES Module but you don't  need to do
+// this if have any bundler (like vite) Or when you initialise the project with npm and has type = "module"
+// in your package.json file. Both ways works fine and better.
