f9b17ec52951478628f6c69ab9bd549643fec26b
Utkarsh Shakya <subhashshakya136@gmail.com>
2025-03-21 09:48:35 +0530

    Add usage instructions and dependency installation notes to Express.js example

--- 03_ExpressJs/index.js
+++ 03_ExpressJs/index.js
@@ -13,3 +13,8 @@ app.use("/", (req, res) => {
 app.listen("8000", () => {
     console.log("Express is running at localhost:8000")
 })
+
+// Now run this file with express or nodemon (recommended) and see the result
+// Make sure you have installed dependencies by running `npm i`
+// You can simple run by `nodemon file_name` in the terminal and see the result on the 
+// given port in the browser or in the postman.
